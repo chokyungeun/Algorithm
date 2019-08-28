5a3f16822551d6caf44bc028cd94736cf20a82d6
조경은 <ruddms0127@gmail.com>
2019-08-28 14:50:06 +0900

    0828

--- src/imtest_0828/Solution_IM_0001__서울9반_조경은.java
+++ src/imtest_0828/Solution_IM_0001__서울9반_조경은.java
@@ -1,10 +1,14 @@
 package imtest_0828;
 
+import java.io.FileInputStream;
+import java.util.Scanner;
+
 public class Solution_IM_0001__서울9반_조경은 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception {
+		System.setIn(new FileInputStream("res/input_im_0828.txt"));
+		Scanner sc = new Scanner(System.in);
+		
 	}
 
 }
