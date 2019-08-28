502d44a1d219f7bb60d280d74d0f42e7a11fa109
조경은 <ruddms0127@gmail.com>
2019-08-28 12:34:52 +0900

    0828

--- src/graph/UnionFind.java
+++ src/graph/UnionFind.java
@@ -5,17 +5,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class UnionFind {
-	public static int N=7;
-	public static int INF=Integer.MAX_VALUE/2;
-	public static int[][] a={
-		{  0, 67,INF, 28, 17,INF, 12},
-		{ 67,  0,INF, 24, 62,INF,INF},
-		{INF,INF,  0,INF, 20, 37,INF},
-		{ 28, 24,INF,  0,INF,INF, 13},
-		{ 17, 62, 20,INF,  0, 45, 73},
-		{INF,INF, 37,INF, 45,  0,INF},
-		{ 12,INF,INF, 13, 73,INF,  0},
-	};
+	
 	public static class Edge implements Comparable<Edge>{
 		int a;
 		int b;
@@ -96,20 +86,6 @@ public class UnionFind {
 	}
 
 }
-
-
-
-/*
-..........[6]
-.....13.....73..12
-[3].28.............[0]
-..24............67.17
-....[1].62.........[4]
-...................20
-...............45....
-..........[5]..37..[2]      
-*/
-
 
 	
 
