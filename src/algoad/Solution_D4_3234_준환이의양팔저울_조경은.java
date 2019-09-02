72a44f5faa5e8da821731da9623d705a2694cdd9
KYUNGEUN CHO <KYUNGEUN CHO@70.12.226.152>
2019-09-02 19:42:37 +0900

    0902

---
+++ src/algoad/Solution_D4_3234_준환이의양팔저울_조경은.java
@@ -0,0 +1,43 @@
+package algoad;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+public class Solution_D4_3234_준환이의양팔저울_조경은 {
+	
+	public static void main(String[] args) throws Exception {
+		System.setIn(new FileInputStream("res/input_d4_3234.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for(int t=1; t<=T; t++) {
+			int N = Integer.parseInt(br.readLine());
+			boolean[] v = new boolean[N];
+			int[] w = new int[N];
+			String[] s = br.readLine().split(" ");
+			for(int i=0; i<s.length; i++) {
+				w[i] = Integer.parseInt(s[i]);
+			}
+			System.out.println("#" + t + " " + dfs(w, v, 0, 0, 0));
+		}
+		br.close();
+	}
+
+	public static int dfs(int[] w, boolean[] v, int left, int right, int depth) {
+		if(left<right) return 0;
+		int r = 0;
+		if(w.length == depth) {
+			return 1;
+		}
+		for(int i=0; i<w.length; i++) {
+			if(!v[i]) {
+				v[i] = true;
+				r = r+dfs(w, v, left+w[i], right, depth+1);
+				r = r+dfs(w, v, left, right+w[i], depth+1);
+				v[i] = false;
+			}
+		}
+		return r;
+	}
+
+}
