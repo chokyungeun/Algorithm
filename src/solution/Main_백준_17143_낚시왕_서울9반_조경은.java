891285ab409262fb96700c5c4340d532efb3c33c
KYUNGEUN CHO <KYUNGEUN CHO@70.12.226.152>
2019-09-04 18:14:56 +0900

    0904

---
+++ src/solution/Main_백준_17143_낚시왕_서울9반_조경은.java
@@ -0,0 +1,18 @@
+package solution;
+
+import java.util.*;
+import java.io.*;
+
+public class Main_백준_17143_낚시왕_서울9반_조경은 {
+
+	public static void main(String[] args) throws Exception{
+		System.setIn(new FileInputStream("res/input_bj_17143.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for(int t=1; t<=T; t++) {
+			
+		}
+
+	}
+
+}
