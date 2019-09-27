3d31309fb97bae6d9af9b049f363b580fae21ea2
yeonkyuu <skehgowh92@naver.com>
2019-09-27 09:04:11 +0900

    0927

--- src/a0926/Solution_D4_4301_콩많이심기_서울9반_조경은.java
+++ src/a0926/Solution_D4_4301_콩많이심기_서울9반_조경은.java
@@ -10,23 +10,35 @@ import java.util.StringTokenizer;
 
 public class Solution_D4_4301_콩많이심기_서울9반_조경은 {
 	public static void main(String[] args) throws Exception {
-		System.setIn(new FileInputStream("res/input_d4_4301.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		for (int t = 1; t <= T; t++) {
-			String[] s = br.readLine().split(" ");
-			int N = Integer.parseInt(s[0]);
-			int M = Integer.parseInt(s[1]);
-			
-			int res = 0;
-			for(int i=0; i<N; i++) {
-				for(int j=0; j<M; j++) {
-					
-				}
-			}
-			
-			
-			System.out.println("#" + t + " " + res);
-		}
-	}
+        int T = Integer.parseInt(br.readLine());
+        for(int t=1; t<=T; t++) {
+            String[] s = br.readLine().split(" ");
+            int M = Integer.parseInt(s[0]);
+            int N = Integer.parseInt(s[1]);
+             
+            int[][] arr = new int[N][M];
+            boolean[][] v = new boolean[N][M];
+            int res = 0;
+            for(int i=0; i<N; i++) {
+                for(int j=0; j<M; j++) {
+                    if(!v[i][j]) {
+                        arr[i][j] = 1;
+                        if(i+2<N)
+                            v[i+2][j] = true;
+                        if(j+2<M)
+                            v[i][j+2] = true;
+                    }
+                }
+            }
+            for(int i=0; i<N; i++) {
+                for(int j=0; j<M; j++) {
+                    if(arr[i][j] == 1)
+                        res++;
+                }
+            }
+             
+            System.out.println("#" + t + " " + res);
+        }
+    }
 }
