0f9f26bd224563e601a3c2fcdd927ad0f244235e
yeonkyuu <skehgowh92@naver.com>
2019-10-04 18:02:06 +0900

    1004

--- src/a1004/Main_정올_1082_화염에서탈출_서울9반_조경은.java
+++ src/a1004/Main_정올_1082_화염에서탈출_서울9반_조경은.java
@@ -6,30 +6,43 @@ import java.util.*;
 public class Main_정올_1082_화염에서탈출_서울9반_조경은 {
 	public static int[] di = {-1, 1, 0, 0};
 	public static int[] dj = {0,0,-1, 1};
-	public static int R,C,res, res2;
+	public static int R,C,res,rr,cc;
 	public static char[][] arr;
-	public static boolean[][] v;
+	public static boolean[][] v,v2;
+	public static ArrayList<Integer> list;
 	
 	public static void dfs(int i, int j) {
-		if(arr[i][j]=='D') {
-			res2 = res;
+		if(i==rr && j==cc) {
+			list.add(res);
 			return;
 		}
 		v[i][j] = true;
 		res++;
-		for(int k=0; k<di.length; k++) {
-			int ni = i + di[k];
-			int nj = j + dj[k];
-			if(ni>=0 && ni<R && nj>=0 && nj<C && arr[ni][nj]=='.') {
-				arr[ni][nj] = '*';
+		v2 = new boolean[R][C];
+		for(int r=0; r<R; r++) {
+			for(int c=0; c<C; c++) {
+				if(arr[r][c] == '*' && v2[r][c] == false) {
+					v2[r][c]=true;
+					for(int k=0; k<di.length; k++) {
+						int ni = r + di[k];
+						int nj = c + dj[k];
+						if(ni>=0 && ni<R && nj>=0 && nj<C && arr[ni][nj]=='.') {
+							arr[ni][nj] = '*';
+							v2[ni][nj]=true;
+						}
+					}
+				}
+					
 			}
 		}
+		
 		
 		for(int k=0; k<di.length; k++) {
 			int ni = i + di[k];
 			int nj = j + dj[k];
-			if(ni>=0 && ni<R && nj>=0 && nj<C && !v[ni][nj] && arr[ni][nj] != '*' && arr[ni][nj]!='X') {
+			if(ni>=0 && ni<R && nj>=0 && nj<C && !v[ni][nj] && (arr[ni][nj] == '.' || arr[ni][nj]=='D')) {
 				arr[i][j] = '.';
+				arr[ni][nj]='S';
 				dfs(ni, nj);
 			}
 		}
@@ -44,6 +57,7 @@ public class Main_정올_1082_화염에서탈출_서울9반_조경은 {
 		C = Integer.parseInt(s[1]);
 		arr = new char[R][C];
 		v = new boolean[R][C];
+		
 		for(int i=0; i<R; i++) {
 			arr[i] = br.readLine().toCharArray();
 		}
@@ -55,18 +69,23 @@ public class Main_정올_1082_화염에서탈출_서울9반_조경은 {
 					r=i;
 					c=j;
 				}
+				if(arr[i][j]=='D') {
+					rr=i;
+					cc=j;
+				}
 				
 			}
 		}
 		res = 0;
-		res2 = 0;
+		list = new ArrayList<>();
 		dfs(r,c);
-		if(res2 == 0) {
+		Collections.sort(list);
+		if(list.size()==0) {
 			System.out.println("impossible");
 		}
 		else {
 			
-			System.out.println(res2);
+			System.out.println(list.get(0));
 		}
 		
 
