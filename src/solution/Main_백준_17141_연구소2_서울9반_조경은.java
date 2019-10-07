8893c1cf092d93871090ccb36b7354d51f179d4a
KYUNGEUN <ruddms0127@naver.com>
2019-10-07 19:05:54 +0900

    1007

--- src/solution/Main_백준_17141_연구소2_서울9반_조경은.java
+++ src/solution/Main_백준_17141_연구소2_서울9반_조경은.java
@@ -1,16 +1,18 @@
 package solution;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
-public class Main_백준_14502_연구소_서울9반_조경은 {
+public class Main_백준_17141_연구소2_서울9반_조경은 {
 	public static int N, M, answer;
 	public static int[][] arr, temp;
 	public static ArrayList<int[]> virus;
 	public static boolean[][] v;
 	public static int[] di = { -1, 1, 0, 0 };
 	public static int[] dj = { 0, 0, -1, 1 };
-
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -21,40 +23,17 @@ public class Main_백준_14502_연구소_서울9반_조경은 {
 		virus = new ArrayList<>();
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < M; j++) {
+			for (int j = 0; j < N; j++) {
 				arr[i][j] = Integer.parseInt(st.nextToken());
 				if (arr[i][j] == 2)
 					virus.add(new int[] { i, j });
 			}
 		}
-		v = new boolean[N][N];
-		answer = 0;
 		
-		
-		
-		System.out.println(answer);
-	}
-
-	private static void vir(int i, int j, int cnt) {
 		
 		
 		
 		
 	}
 
-	private static void bfs(int i, int j) {
-		Queue<int[]> q = new LinkedList<>();
-		q.offer(new int[] { i, j });
-		while (!q.isEmpty()) {
-			int[] curr = q.poll();
-			for (int k = 0; k < di.length; k++) {
-				int ni = curr[0] + di[k];
-				int nj = curr[1] + dj[k];
-				if (ni >= 0 && ni < N && nj >= 0 && nj < M && temp[ni][nj] == 0) {
-					temp[ni][nj] = 2;
-					q.offer(new int[] { ni, nj });
-				}
-			}
-		}
-	}
 }
