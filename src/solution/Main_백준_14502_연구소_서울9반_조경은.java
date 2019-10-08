0bcc25d191d8a363c2eb70ea0635ebcfb114fd86
KYUNGEUN <ruddms0127@naver.com>
2019-10-08 13:55:35 +0900

    1008

--- src/solution/Main_백준_14502_연구소_서울9반_조경은.java
+++ src/solution/Main_백준_14502_연구소_서울9반_조경은.java
@@ -16,8 +16,8 @@ public class Main_백준_14502_연구소_서울9반_조경은 {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		arr = new int[N][N];
-		temp = new int[N][N];
+		arr = new int[N][M];
+		temp = new int[N][M];
 		virus = new ArrayList<>();
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -27,19 +27,52 @@ public class Main_백준_14502_연구소_서울9반_조경은 {
 					virus.add(new int[] { i, j });
 			}
 		}
-		v = new boolean[N][N];
+		v = new boolean[N][M];
 		answer = 0;
-		
-		
-		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (arr[i][j] == 0)
+					wall(i, j, 1);
+			}
+		}
 		System.out.println(answer);
 	}
 
-	private static void vir(int i, int j, int cnt) {
-		
-		
-		
-		
+	private static void wall(int i, int j, int cnt) {
+		arr[i][j] = 1;
+		v[i][j] = true;
+
+		if (cnt == 3) {
+			for (int a = 0; a < N; a++) {
+				temp[a] = arr[a].clone();
+			}
+			for (int k = 0; k < virus.size(); k++) {
+				bfs(virus.get(k)[0], virus.get(k)[1]);
+			}
+			int res = 0;
+			for (int a = 0; a < N; a++) {
+				for (int b = 0; b < M; b++) {
+					if (temp[a][b] == 0) {
+						res++;
+					}
+				}
+			}
+			v[i][j] = false;
+			arr[i][j] = 0;
+			answer = Math.max(answer, res);
+			return;
+		}
+
+		for (int k = 0; k < N; k++) {
+			for (int l = 0; l < M; l++) {
+				if (arr[k][l] == 0 && !v[k][l]) {
+					wall(k, l, cnt + 1);
+
+				}
+			}
+		}
+		v[i][j] = false;
+		arr[i][j] = 0;
 	}
 
 	private static void bfs(int i, int j) {
