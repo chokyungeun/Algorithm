5de0ad77e0632f2430efdc3fe23c7aad97decb53
KYUNGEUN CHO <KYUNGEUN CHO@172.30.56.244>
2019-10-13 01:25:00 +0900

    1013

--- src/Test.java
+++ src/Test.java
@@ -4,10 +4,13 @@ import java.util.*;
 public class Test {
 
 	public static void main(String[] args) throws Exception {
-		ArrayList<Character> cname = new ArrayList<>();
-		cname.add('A');
-		cname.add('d');
-		cname.add('f');
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		
+		
+		
 		
 	}
 
