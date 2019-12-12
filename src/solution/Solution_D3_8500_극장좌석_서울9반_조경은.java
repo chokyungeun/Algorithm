50fbf41d33ecb6708a2a73e06fab605b5b9b7785
KYUNGEUN CHO <KYUNGEUN CHO@192.168.0.2>
2019-12-12 20:49:41 +0900

    극장좌석

---
+++ src/solution/Solution_D3_8500_극장좌석_서울9반_조경은.java
@@ -0,0 +1,24 @@
+package solution;
+import java.io.*;
+import java.util.*;
+
+public class Solution_D3_8500_극장좌석_서울9반_조경은 {
+	public static int N;
+	
+	public static void main(String args[]) throws Exception {
+		System.setIn(new FileInputStream("res/input_d3_8500.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int T = Integer.parseInt(br.readLine());
+		for(int t=1; t<=T; t++) {
+			N = Integer.parseInt(br.readLine());
+			st = new StringTokenizer(br.readLine());
+			for(int i=0; i<N; i++) {
+				
+			}
+			
+			
+		}
+		
+	}
+}
