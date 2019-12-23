f6cd93093ab028478defa064b7865d6668d27552
KYUNGEUN CHO <KYUNGEUN CHO@70.12.226.152>
2019-12-23 15:32:41 +0900

    1223

--- src/solution/Main_백준_17825_주사위윷놀이_서울9반_조경은.java
+++ src/solution/Main_백준_17825_주사위윷놀이_서울9반_조경은.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class Main_백준_17825_주사위윷놀이_서울9반_조경은 {
 	public static int[] dice;
+	public static int[][][][] map = {};
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
